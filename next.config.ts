8cd593a8b502fff8381709dff35909be4b3aa219
tetsugo-mac <tetsugou2002@gmail.com>
2025-06-22 21:46:18 +0900

    リポジトリ名変更後のビルド

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	/* config options here */
 	output: "export",
-	basePath: process.env.NODE_ENV === "production" ? "/portfolio" : "",
+	basePath: "",
 	images: {
 		unoptimized: true,
 	},
